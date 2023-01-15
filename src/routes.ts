e8d7d2d43a5e89e73671973fb15ae00fc861e749
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-01-15 14:55:08 +0100

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+export const postRegister = '/api/register';
+export const getVerifyEmailToken = '/api/verify-email';
+export const postLogin = '/api/login';
+export const getAuthTest = '/api/auth-test';
+export const postUploadParticipants = '/api/upload-participants';
+export const getParticipants = '/api/participants';
+export const getExperimentConfig = '/api/experiment-config';
+export const postExperimentConfig = '/api/experiment-config';
+export const getExperimentConfigHistory = '/api/experiment-config-history';
+
+export const postCreateSession = '/api/session';
+export const postCheckParticipantCode = '/api/check-participant-code';
+export const getParticipantConfig = '/api/config';
+export const postEvent = '/api/event';
+export const getEvents = '/api/events';
