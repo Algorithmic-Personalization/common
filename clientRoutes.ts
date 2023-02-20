9bb3e14dfb5272c146f6d17bd5141c77e2262cbd
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-02-20 19:22:33 +0100

    moved some routes to server

---
+++ clientRoutes.ts
@@ -0,0 +1,4 @@
+export const postCreateSession = '/api/session';
+export const postCheckParticipantCode = '/api/check-participant-code';
+export const getParticipantConfig = '/api/config';
+export const postEvent = '/api/event';
